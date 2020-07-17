3a0f07f55ca1ce253b863638103b4cfaa8c19ce5
Patro-ss <68190141+Patro-ss@users.noreply.github.com>
2020-07-17 15:07:23 +0200

    Added Binary_calculator
    In this calculator you can bin, oct, hex. Your decimal value

---
+++ BInary_calc.py
@@ -0,0 +1,20 @@
+while True:
+	
+	x = int(input("Enter decimal value: "))
+	choice = input("Enter Choice(bin/oct/hex): ")
+		
+	if choice == 'bin':
+		print("Binary value is: \n", bin(x))
+		
+		break
+	elif choice == 'oct':
+		print("Octal value is: \n", oct(x))
+		
+		break	
+	elif choice == 'hex':
+		print("Hexadecimal value is: \n", hex(x))
+		
+		break
+	else:
+		print("Invalid operation")
+		
