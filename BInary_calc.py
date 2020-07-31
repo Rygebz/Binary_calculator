6114afc5f1abc17cc25f400123048478d884486c
Patro-ss <68190141+Patro-ss@users.noreply.github.com>
2020-07-31 19:45:06 +0200

    Medium changes
    Added option for more options in the selection

--- BInary_calc.py
+++ BInary_calc.py
@@ -1,19 +1,51 @@
-while True:
-	x = int(input("Enter decimal value: "))
+op = 1
+chr(op)
+
+while(op != 'e'):
 	choice = input("Enter Choice(bin/oct/hex): ")
+	x = int(input("Enter decimal value: "))
 		
 	if choice == 'bin':
 		print("Binary value is: \n", bin(x))
-		
-		break
+		print("Are u want to continue, y or n?")
+		choice = input("Enter opertation: ")
+		while True:
+			if choice == 'y': #Mayby do an extra thing i thin big y and big N
+				x = int(input("Enter decimal value: "))
+				print("Binary value is: \n", bin(x))	
+				print("Are u want to continue, y or n?")
+				choice = input("Enter opertation: ")
+			break	
+		if choice == 'n':
+			print(" Enter Choice(bin/oct/hex)")
+			choice = input("Enter operation: ")
 	elif choice == 'oct':
 		print("Octal value is: \n", oct(x))
-		
-		break	
+		print("Are u want to continue, y or n?")
+		choice = input("Enter opertation: ")
+		while True:
+			if choice == 'y': #Mayby do an extra thing i thin big y and big N
+				x = int(input("Enter decimal value: "))
+				print("Octal value is: \n", oct(x))
+				print("Are u want to continue, y or n ?")
+				choice = input("Enter opertation: ")
+			break	
+		if choice == 'n':
+			print(" Enter Choice(bin/oct/hex)")
+			choice = input("Enter operation: ")
 	elif choice == 'hex':
 		print("Hexadecimal value is: \n", hex(x))
-		
-		break
+		print("Are u want to continue, y or n?")
+		choice = input("Enter opertation: ")
+		while True:
+			if choice == 'y': #Mayby do an extra thing i thin big y and big N
+				x = int(input("Enter decimal value: "))
+				print("Hexadecimal value is: \n", hex(x))
+				print("Are u want to continue, Y or N ?")
+				choice = input("Enter opertation: ")
+			break	
+		if choice == 'n':
+			print(" Enter Choice(bin/oct/hex)")
+			choice = input("Enter operation: ")
 	else:
 		print("Invalid operation")
-		
